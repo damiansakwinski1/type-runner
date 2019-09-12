003022084eed22f794fe501113dbb18d02655b37
Adam Polak <polakadam@outlook.com>
2019-09-12 09:38:51 +0200

    Fixed https

--- src/index.js
+++ src/index.js
@@ -1,97 +1,98 @@
-const app = require('express')();
-const fs = require('fs')
-const https = require('https').createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-},app);
-const http = require('http').createServer(app);
+const app = require("express")();
+const fs = require("fs");
+const https = require("https").createServer(
+  {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert")
+  },
+  app
+);
+const http = require("http").createServer(app);
 
-const io = require('socket.io')(process.env.NODE_ENV === 'test' ? http: https);
-const { Subject } = require('rxjs')
-const Handlers = require('./engine/handlers')
-const JoinGame = require('./engine/handler/join-game')
-const LeaveGame = require('./engine/handler/leave-game')
-const PlayerStatus = require('./engine/handler/player-status')
-const Spectate = require('./engine/handler/spectate')
-const Highscores = require('./engine/handler/highscores')
-const Games = require('./engine/games')
+const io = require("socket.io")(process.env.NODE_ENV === "test" ? http : http);
+const { Subject } = require("rxjs");
+const Handlers = require("./engine/handlers");
+const JoinGame = require("./engine/handler/join-game");
+const LeaveGame = require("./engine/handler/leave-game");
+const PlayerStatus = require("./engine/handler/player-status");
+const Spectate = require("./engine/handler/spectate");
+const Highscores = require("./engine/handler/highscores");
+const Games = require("./engine/games");
 
-const messagesToSocketStream$ = new Subject()
+const messagesToSocketStream$ = new Subject();
 
-process.on('unhandledRejection', err => {
-  console.log(err)
-  process.exit(1)
-})
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
 
-process.on('uncaughtException', err => {
-  console.log(err)
-  process.exit(1)
-})
+process.on("uncaughtException", err => {
+  console.log(err);
+  process.exit(1);
+});
 
-const games = new Games()
+const games = new Games();
 
 const handlers = new Handlers({
-  'join-game': new JoinGame(games, messagesToSocketStream$),
-  'player-status': new PlayerStatus(games, messagesToSocketStream$),
-  'spectate': new Spectate(games, messagesToSocketStream$),
-  'highscores': new Highscores(games, messagesToSocketStream$),
-  'leave-game': new LeaveGame(games, messagesToSocketStream$)
-})
+  "join-game": new JoinGame(games, messagesToSocketStream$),
+  "player-status": new PlayerStatus(games, messagesToSocketStream$),
+  spectate: new Spectate(games, messagesToSocketStream$),
+  highscores: new Highscores(games, messagesToSocketStream$),
+  "leave-game": new LeaveGame(games, messagesToSocketStream$)
+});
 
 const filterMessages = messages => message => {
   if (messages.find(filteredMessage => filteredMessage === message.type)) {
-    return false
+    return false;
   }
 
-  return true
-}
+  return true;
+};
 
-const spectatorMessages = filterMessages([
-  'joined-game'
-])
+const spectatorMessages = filterMessages(["joined-game"]);
 
-io.on('connection', socket => {
-  socket.on('message', message => {
+io.on("connection", socket => {
+  socket.on("message", message => {
     //console.log(message)
     handlers.handle({
       ...message,
       socketId: socket.id
-    })
-  })
+    });
+  });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     handlers.handle({
-      type: 'leave-game',
+      type: "leave-game",
       socketId: socket.id
-    })
-  })
+    });
+  });
 });
 
 messagesToSocketStream$.subscribe(message => {
   //console.log(message)
   message.target.forEach(id => {
-    io.to(id).emit('message', {
+    io.to(id).emit("message", {
       type: message.type,
       payload: message.payload
-    })
-  })
+    });
+  });
 
   if (spectatorMessages(message)) {
     games.spectators.forEach(spectator => {
-      io.to(spectator).emit('message', {
+      io.to(spectator).emit("message", {
         type: message.type,
         payload: message.payload
-      })
-    })
+      });
+    });
   }
-})
+});
 
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   http.listen(8443, () => {
-    console.log('listening on port 8443')
-  })
+    console.log("listening on port 8443");
+  });
 } else {
   https.listen(8443, () => {
-    console.log('listening on port 8443')
-  })
+    console.log("listening on port 8443");
+  });
 }
