767afea01980cf04cea098edc5ac27c242bd3dd4
Bartlomiej Dabrowski <bartlomiej.dabrowski@tsh.io>
2019-10-24 10:40:35 +0200

    Waiting for db connection established before app starts

--- src/connection.js
+++ src/connection.js
@@ -1,15 +1,17 @@
 const config = require("../config/db.static");
 const { Pool } = require("pg");
-const db = new Pool({
+const pool = new Pool({
   ...config,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000
 });
 
-db.on("error", (err, client) => {
+pool.on("error", (err, client) => {
   console.error("Unexpected error on idle client", err);
   process.exit(-1);
 });
+
+const db = pool.connect().then(client => client);
 
 module.exports = db;
